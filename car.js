3b0e249a21b8da6be505333eeac9e12741712586
Pratik-singh <pratiksingh8591@gmail.com>
2026-04-10 06:28:24 +0530

    added styling

--- car.js
+++ car.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const app =express();
+const path=require('path');
 const {hostRouter}=require('./routes/host');
 const user=require('./routes/user')
 app.set('view engine','ejs');
 app.set('views','views');
 app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 app.use((req,res,next)=>{
     console.log(req.method,req.url,req.body);
     next();
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs", "./routes/**/*.js", "./car.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
