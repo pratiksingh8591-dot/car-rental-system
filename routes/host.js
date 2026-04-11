977a70c6490140392200448eacab62a96c7ea6b2
Pratik-singh <pratiksingh8591@gmail.com>
2026-04-11 12:45:55 +0530

    added thank you page

--- routes/host.js
+++ routes/host.js
@@ -2,12 +2,12 @@ const express=require('express');
 const hostRouter=express.Router();
 const getcar= [];
 hostRouter.get("/car-book",(req,res,next)=>{
-   res.render('addcar');
+   res.render('addcar',{content:'ADD CAR AND GET GREAT DEALS'});
 })
 hostRouter.post("/car-book",(req,res,next)=>{
      const name=req.body.carName;
      const cno=req.body.carNo;
      getcar.push({name,cno})
-     res.redirect('/');
+     res.render('caradded',{content:'thank you for adding'});
 })
 module.exports={hostRouter,getcar};
